51fc3fd07f1a00432851d4a2a6e27569fe47d2df
fwcd <fwcdmail@gmail.com>
2021-03-18 22:16:26 +0100

    Add (LSP) language client

---
+++ src/extension.ts
@@ -0,0 +1,26 @@
+import * as vscode from 'vscode';
+import { LanguageClient, LanguageClientOptions, RevealOutputChannelOn, ServerOptions } from 'vscode-languageclient/node';
+
+export function activate(context: vscode.ExtensionContext) {
+    const config = vscode.workspace.getConfiguration('clio');
+
+    if (config.get('languageServer.enabled')) {
+        const serverOptions: ServerOptions = {
+            command: config.get('languageServer.command'),
+            options: {
+                shell: true,
+                cwd: config.get('languageServer.cwd') || null
+            }
+        };
+        const clientOptions: LanguageClientOptions = {
+            documentSelector: [
+                { language: 'clio', scheme: 'file' }
+            ],
+            outputChannelName: 'Clio',
+            revealOutputChannelOn: RevealOutputChannelOn.Never
+        };
+        const client = new LanguageClient('Clio', serverOptions, clientOptions);
+
+        context.subscriptions.push(client.start());
+    }
+}
